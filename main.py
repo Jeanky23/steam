d7669a3486fc9f3fdc5f7d238c312744fdc2e07a
Jeanky23 <116356015+Jeanky23@users.noreply.github.com>
2022-11-29 00:56:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from profile import Profile
 
 def main():
-    profile1 = Profile(1,"Jeanky","jean@gmial.com","qwer132")
-    profile1.print2()
-    profile1.changepass()
-    profile1.print2()
+    profile1 = Profile(15445,"Jeanky","jean@gmail.com","qwer132")
+    profile1.show()
+    profile1.change_pass()
+    profile1.show()
 main()
