86df70a727f7df3b5d2f31c2d005e7ed5a291be8
Jeanky23 <116356015+Jeanky23@users.noreply.github.com>
2022-11-29 00:57:45 -0300

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,13 +1,13 @@
 class Profile:
     def __init__(self,id,username,email,password,status="online",level=0):
-        self.id=id
-        self.username=username
-        self.email=email
-        self.password=password
-        self.status=status
-        self.level=level
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+        self.status = status
+        self.level =  level
 
-    def print2(self):
+    def show(self):
         print("El id es: ",self.id)
         print("El nombre de usuario es: ",self.username)
         print("El email es: ",self.email)
@@ -15,12 +15,5 @@ class Profile:
         print("Estado: ",self.status)
         print("Nivel: ",self.level)
 
-    def changepass(self,new_password):
-        self.username=input("Ingrese su nombre de usuario: ")
-        self.password=input("Ingrese su contraseña: ")
-        self.new_password=input("Para cambiar contraseña, ingrese nueva contraseña: ")
-
-        if (self.new_password == self.password):
-            print("Favor de elejir una contraseña distinta a la anterior.")
-            else:
-            (self.password = self.new_password)
+    def change_pass(self):
+        self.password = input("Ingrese nueva contraseña: ")
